3b4c673fca9325e24835e0492e86f8dd0ddadb92
Lukas Buenger <lukasbuenger@gmail.com>
2018-08-27 11:18:46 +0200

    port new signout components

--- components/Auth/Me.js
+++ components/Auth/Me.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {compose} from 'redux'
+import { compose } from 'react-apollo'
 
 import withMe from '../../lib/withMe'
 import withT from '../../lib/withT'
@@ -8,19 +8,17 @@ import SignIn from './SignIn'
 import SignOut from './SignOut'
 
 const Me = ({me, t, email}) => (
-  <div>
-    {me ? (
-      <div>
-        {t('me/signedinAs', {
-          nameOrEmail: (me.name && me.name.trim()) || me.email
-        })}
-        <br />
-        <SignOut />
-      </div>
-    ) : (
-      <SignIn email={email} />
-    )}
-  </div>
+  me ? (
+    <div>
+      {t('me/signedinAs', {
+        nameOrEmail: me.name ? `${me.name.trim()} (${me.email})` : me.email
+      })}
+      <br />
+      <SignOut />
+    </div>
+  ) : (
+    <SignIn email={email} />
+  )
 )
 
 export default compose(
