d86bee6848faf446f758e3d7c1b5240ac647515f
Lukas Buenger <lukasbuenger@gmail.com>
2018-10-01 17:27:10 +0200

    basic dynamic component. wip.

---
+++ components/Editor/plugins/dynamicComponent/index.js
@@ -0,0 +1,39 @@
+import { cloneElement } from 'react'
+import { isBlock } from '../../base/lib'
+import { SchemaComponent } from '../../base/components/Schema'
+import EditOverlay from './EditOverlay'
+
+export default {
+  renderNode (props) {
+    const { node } = props
+    if ((isBlock('dynamicComponent'), node)) {
+      const data = node.data.toJS()
+      const component = (
+        <SchemaComponent
+          name='dynamicComponent'
+          showException
+          key={JSON.stringify(data)}
+          {...data}
+        />
+      )
+      const preview = cloneElement(component, {
+        raw: true
+      })
+
+      return (
+        <EditOverlay
+          {...props}
+          component={component}
+          preview={preview}
+        />
+      )
+    }
+  },
+  schema: {
+    blocks: {
+      dynamicComponent: {
+        isVoid: true
+      }
+    }
+  }
+}
