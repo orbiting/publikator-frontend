e7de12b5f052199cd3ed2f8c5ffe0cd88b5bff4e
Lukas Buenger <lukasbuenger@gmail.com>
2018-10-01 09:23:19 +0200

    remove query id to avoid apollo cache conflicts

--- components/Search/RepoSearch.js
+++ components/Search/RepoSearch.js
@@ -7,34 +7,33 @@ import debounce from 'lodash.debounce'
 import { GITHUB_ORG, REPO_PREFIX } from '../../lib/settings'
 
 export const filterRepos = gql`
-query searchRepo($after: String, $search: String) {
-  repos(first: 10, after: $after, search: $search) {
-    totalCount
-    pageInfo {
-      endCursor
-      hasNextPage
-    }
-    nodes {
-      id
-      latestCommit {
+  query searchRepo($after: String, $search: String) {
+    repos(first: 10, after: $after, search: $search) {
+      totalCount
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      nodes {
         id
-        document {
-          id
-          meta {
-            title
-            image
-            description
-            subject
-            credits
-            kind
-            color
-            format {
-              id
-              repoId
-              meta {
-                title
-                color
-                kind
+        latestCommit {
+          document {
+            meta {
+              title
+              image
+              description
+              subject
+              credits
+              kind
+              color
+              format {
+                id
+                repoId
+                meta {
+                  title
+                  color
+                  kind
+                }
               }
             }
           }
@@ -42,40 +41,49 @@ query searchRepo($after: String, $search: String) {
       }
     }
   }
-}
 `
 
 const ConnectedAutoComplete = graphql(filterRepos, {
   skip: props => !props.filter,
   options: ({ search }) => ({ variables: { search: search } }),
-  props: (props) => {
+  props: props => {
     if (props.data.loading) return { data: props.data, items: [] }
-    const { data: { repos: { nodes = [] } = {} } } = props
-    return ({
+    const {
+      data: { repos: { nodes = [] } = {} }
+    } = props
+    return {
       data: props.data,
       items: nodes.map(v => ({
         value: v,
-        text: v.latestCommit.document.meta.title ||
+        text:
+          v.latestCommit.document.meta.title ||
           v.id.replace([GITHUB_ORG, REPO_PREFIX || ''].join('/'), '')
       }))
-    })
+    }
   }
 })(props => {
   const showLoader = props.data && props.data.loading
   return (
     <span style={{ position: 'relative', display: 'block' }}>
       <Autocomplete key='autocomplete' {...props} />
-      {!!showLoader && <span style={{ position: 'absolute', top: '21px', right: '0px', zIndex: 500 }}>
-        <InlineSpinner size={35} />
-      </span>}
+      {!!showLoader && (
+        <span
+          style={{
+            position: 'absolute',
+            top: '21px',
+            right: '0px',
+            zIndex: 500
+          }}
+        >
+          <InlineSpinner size={35} />
+        </span>
+      )}
     </span>
   )
 })
 
 const safeValue = value =>
-  typeof value === 'string'
-    ? { value, text: value }
-    : null
+  typeof value === 'string' ? { value, text: value } : null
 
 export default class RepoSearch extends Component {
   constructor (props) {
@@ -89,7 +97,10 @@ export default class RepoSearch extends Component {
 
     this.filterChangeHandler = this.filterChangeHandler.bind(this)
     this.changeHandler = this.changeHandler.bind(this)
-    this.setSearchValue = debounce(this.setSearchValue.bind(this), 500)
+    this.setSearchValue = debounce(
+      this.setSearchValue.bind(this),
+      500
+    )
   }
 
   componentWillReceiveProps (nextProps) {
